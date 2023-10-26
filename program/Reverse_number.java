f9d2fb061ab851962f5793c7d7c36d2ae3fb58cd
vedashree <vedashreepatil07@gmail.com>
2023-10-26 20:33:55 +0530

    initial Commit.

---
+++ program/Reverse_number.java
@@ -0,0 +1,18 @@
+package program;
+
+public class Reverse_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 987654, reverse = 0;  
+		while(number != 0)   
+		{  
+		int remainder = number % 10;  
+		reverse = reverse * 10 + remainder;  
+		number = number/10;  
+		}  
+		System.out.println("The reverse of the given number is: " + reverse);
+
+	}
+
+}
